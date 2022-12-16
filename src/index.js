04577c5197249cd777f7246ed0a4cfe671b2e5f1
daveCC_ <shelkanife884@gmail.com>
2022-12-16 11:57:09 -0600

    fixed public route

--- src/index.js
+++ src/index.js
@@ -24,8 +24,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 socket(io);
 
-app.use("/", require("./routes/index"));
 app.use("/public", express.static(path.join(__dirname, "public")));
+app.use("/", require("./routes/index"));
 
 server.listen(app.get("port"));
 
